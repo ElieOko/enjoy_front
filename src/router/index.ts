d93f8f90e26525aede500531b7e7e1ef5956621b
ElieOko <elieoko100@gmail.com>
2024-06-22 15:28:46 +0100

    routage

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
+import SiteView from '@/views/site/SiteView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,13 +15,20 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: LoginView,
+      meta: { layout: 'empty' }
     },
     {
       path: '/register',
       name: 'register',
-      component: RegisterView
+      component: RegisterView,
+      meta: { layout: 'empty' }
     },
+    {
+      path: '/site',
+      name: 'site',
+      component: SiteView
+    }
   ]
 })
 
